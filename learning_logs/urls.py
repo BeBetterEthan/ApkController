6d8ddaaea1bc1386d4fc1b621b2750cd325233d8
Jacob <xueyong07@163.com>
2018-11-08 01:15:03 +0800

    initial project

---
+++ learning_logs/urls.py
@@ -0,0 +1,14 @@
+"""定义learning_logs的URL模式"""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('topic/', views.topics, name='topics'),
+    path('topic/<topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    path('new_entry/<topic_id>)/', views.new_entry, name='new_entry'),
+    path('edit_entry/<entry_id>)/', views.edit_entry, name='edit_entry'),
+]
